9d1fa4d9cc4fd3b64de37a5fa64b23bf8a8d7316
Fezan Muhammad Ali <callmetheengcountio@gmail.com>
2024-01-08 13:11:33 +0500

    had used Navigate component from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Route } from "react-router";
 import Homepage from "./pages/Homepage";
@@ -42,10 +42,7 @@ function App() {
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="app" element={<AppLayout />}>
-            <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate replace to="cities" />} />
             <Route
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
